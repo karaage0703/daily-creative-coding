3100c0db911a80a08bd4d294507431dad362061d
karaage0703 <coldchicken@gmail.com>
2021-11-26 09:33:05 +0900

    Initial commit

---
+++ 20211126_001_first/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    // createCanvas(963, 633);
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    angleMode(DEGREES);
+}
+
+function draw() {
+    fill(255, 255, 255);
+    circle(width / 2, height / 2, height / 2);
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # daily-creative-coding
 Daily Creative Coding
+
+
+# References
+- https://yoppa.org/sfc-design21/10515.html
