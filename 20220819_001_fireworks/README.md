74871b051400c0efa75fe484271c685a3d9fcc23
karaage0703 <coldchicken@gmail.com>
2022-08-19 11:38:46 +0900

    Add sample

---
+++ 20220819_001_fireworks/README.md
@@ -0,0 +1,6 @@
+# Fire Works
+Fire Works
+
+# References
+- https://paiza.hatenablog.com/entry/2022/08/17/143000
+- https://github.com/crashmax-dev/fireworks-js/#fireworks-jsweb 
---
+++ 20220819_001_fireworks/sketch.js
@@ -0,0 +1,5 @@
+import { Fireworks } from 'https://cdn.skypack.dev/fireworks-js';
+const container = document.getElementById('container');
+const fireworks = new Fireworks(container);
+
+fireworks.start();
